2b4b998c1525dd5d66825a99422f6ad060838cd2
Ufuomsky95 <orifeufuoma@gmail.com>
2025-07-09 16:49:47 +0100

    my latest 4:49pm

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
   
     <BrowserRouter>
