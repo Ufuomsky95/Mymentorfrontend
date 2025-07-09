882429ec385123e29ac93b6d9cea0321fdb8934c
Ufuomsky95 <orifeufuoma@gmail.com>
2025-07-09 17:29:51 +0100

    hero2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 
 function Home() {
   return (
-    <div className=' bg-purple-200 p-5'>
+    <div className=' bg-purple-200 p-5 justify-center text-center text-3xl p-5 max-auto'>
         Welcome to my Mentorship Class Home Page 
       <a href="#login"> <button type='submit' className='right-3 mt-5 text-2xl bg-purple-400 hover:bg-purple-700 p-3 rounded-xl top-9 focus:outline-none w-20 '>Login</button></a>
          <h1 className='text-white font-bold justify-center text-center text-3xl p-20 max-auto'>"You Don’t Have to Figure It Out Alone."</h1>
-         <h2>Find a Mentor. Be a Mentor.</h2>
-<p>Real growth starts with real guidance — connect with experienced mentors who’ve walked your path and want to see you thrive. <br />
+         <h2  className= 'font-bold justify-center text-center text-3xl p-5 max-auto'>Find a Mentor. Be a Mentor.</h2>
+<p className='text-white justify-center text-center text-3xl max-auto' >Real growth starts with real guidance — connect with experienced mentors who’ve walked your path and want to see you thrive. <br />
 Get Started * Find Your Match</p>  
 
     </div>
