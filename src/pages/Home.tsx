61535c9ee2df9002d45e92ffef2fccc63b702035
Ufuomsky95 <orifeufuoma@gmail.com>
2025-07-09 15:33:38 +0100

    Latest commit 9th July

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
          <h1 className='text-white font-bold justify-center text-center text-3xl p-20 max-auto'>"You Don’t Have to Figure It Out Alone."</h1>
          <h2>Find a Mentor. Be a Mentor.</h2>
 <p>Real growth starts with real guidance — connect with experienced mentors who’ve walked your path and want to see you thrive. <br />
-Get Started * Find Your Match</p>
+Get Started * Find Your Match</p>  
     </div>
    
   )
