0e722c1db4ec908625856b3884f44c534077410d
Ufuomsky95 <orifeufuoma@gmail.com>
2025-07-17 13:25:47 +0100

    update 17th

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,11 @@ function Home() {
          <h1 className='text-white font-bold justify-center text-center text-6xl pt-15 pr-15 pl-15 pb-5  max-auto'>"You Don’t Have to Figure It Out Alone."</h1>
        <h2  className= 'font-bold justify-center text-center text-3xl  max-auto'>Find a Mentor. Be a Mentor.</h2>
  <p className='text-white justify-center text-center text-2xl max-auto p-5' >Real growth starts with real guidance — connect with experienced mentors who’ve walked your path and want to see you thrive. <br />
- Get Started * Find Your Match</p>  
+
+  <a href="http://localhost:5173/login"> <button className='right-3 mt-5 text-2xl bg-purple-400 hover:bg-purple-700 p-3 rounded-xl top-9 focus:outline-none w-28 '>Get Started</button></a> 
+
+  <a href="http://localhost:5173/login"> <button className='right-3 mt-5 text-2xl bg-purple-400 hover:bg-purple-700 p-3 rounded-xl top-9 focus:outline-none w-35 '>Find Your Match</button></a> 
+</p>  
 
    </div>
   
